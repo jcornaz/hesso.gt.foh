a6dd51dd8845171c2ee2e9e18fe37f2600d6f435
Alshweiki <ali.shwaiky@gmail.com>
2016-06-03 11:01:02 +0200

    lose level added

---
+++ game/Assets/FoH/Scripts/lose.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+
+function OnTriggerEnter(col: Collider)
+{
+	if(col.gameObject.name == "Plane lose")
+	{
+		transform.position = Vector3(521.4 , 93.53, 426.45);
+	}
+
+}
--- game/Assets/FoH/Scripts/menu.js
+++ game/Assets/FoH/Scripts/menu.js
@@ -8,16 +8,16 @@ function Update () {
 
 }
 function easyClick()
-{
-
+{	
+	transform.position = Vector3(518.012, 21.1816, 401.624);
 }
 function mediumClick()
 {
-
+	transform.position = Vector3(463.6601 , 63.8708, 374.0429);
 }
 function hardClick()
 {
-
+	transform.position = Vector3(506.02,82.6933, 412.94);
 }
 
 function winResult()
