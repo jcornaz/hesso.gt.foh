5923ec5f06a1ded32c6637cafab05600c21ddf89
Alshweiki <ali.shwaiky@gmail.com>
2016-06-03 10:43:49 +0200

    menu click text

--- game/Assets/FoH/Scripts/menu.js
+++ game/Assets/FoH/Scripts/menu.js
@@ -7,3 +7,24 @@ function Start () {
 function Update () {
 
 }
+function easyClick()
+{
+
+}
+function mediumClick()
+{
+
+}
+function hardClick()
+{
+
+}
+
+function winResult()
+{
+
+}
+function loseResult()
+{
+
+}
