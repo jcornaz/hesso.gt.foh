da898c956f2f399eec3337e84b8d73a4692325b9
Alshweiki <ali.shwaiky@gmail.com>
2016-06-05 10:36:32 +0200

    bar process

---
+++ game/Assets/FoH/Scripts/VRCharacter/barProcess.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+
+
+
+ var progress : float = 0;
+var pos : Vector2 = new Vector2(20,40);
+var size : Vector2 = new Vector2(60,20);
+var progressBarEmpty : Texture2D;
+var progressBarFull : Texture2D;
+
+ function OnGUI()
+ {
+ 
+    GUI.DrawTexture(Rect(pos.x, pos.y, size.x, size.y), progressBarEmpty);
+    GUI.DrawTexture(Rect(pos.x, pos.y, size.x * Mathf.Clamp01(progress), size.y), progressBarFull);
+ }
+
+
+function Start () {
+
+}
+
+function Update()
+ {
+     progress = Time.time * 0.05;
+ }
