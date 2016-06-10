7936119e43264ec6a8383a7d696e5c71bd634f46
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2016-06-10 08:51:10 +0200

    Make the bird clickable

---
+++ game/Assets/DestroyOnClick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnClick : MonoBehaviour {
+    
+	public void Click()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
